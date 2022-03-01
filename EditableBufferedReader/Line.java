bf8fa77af0534e15568937e8a31bd512d5013736
Marina-Morgado <marina2000987@gmail.com>
2022-03-01 12:26:03 +0100

    classe line

--- EditableBufferedReader/Line.java
+++ EditableBufferedReader/Line.java
@@ -3,5 +3,27 @@
  * @author marcnueno
  */
 public class Line {
-    String line ="";
+    String line;
+    public int cursor;
+
+    public Line(){
+        line = "";
+        cursor = 0;
+    }
+
+    public void add (char c){
+        
+        //cursor = cursor +1;
+    }
+
+    public void delete (){
+
+    }
+
+    public void CursorBegin (){
+        cursor = 0;
+    }
+    public void CursorEnd(){
+        
+    }
 }
